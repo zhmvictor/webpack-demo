1cab3d9cb54f43464d56f07b793a299fb052c35c
zhouhuimin <zhouhuimin@ir-soft.com.cn>
2020-05-30 11:25:51 +0800

    管理资源和输出

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # webpack-demo
-create for study webapck
+
+## 起步
+
+`npm init -y`: 生成 `package.json`文件
+
+`npm install webpack webpack-cli -D`: 安装依赖， `webpack-cli`使我们可以在命令行执行webpack命令
+
+
+## 加载资源文件
+
+### css
+
+css-loader、style-loader
+
+### image/字体
+
+file-loader
+
+### 数据资源
+
+webpack内置支持打包JSON文件
+
+csv/tsv: csv-loader
+
+xml: xml-loader
+
+
+## 管理输出
+
+### html-webpack-plugin
+
+webpack的index.html不能自动更新引入的入口文件名称，`html-webpack-plugin`插件用于解决这个问题，可以向 index.html 动态添加 bundle
+
+
+### clean-webpack-plugin
+
+每次重新打包构建前清理无用文件
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,71 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        // 加载 css
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+              esModule: true,
+            },
+          },
+          'css-loader', // style-loader
+        ]
+      },
+      {
+        // 加载图片
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader'
+        ],
+      },
+      {
+        // 加载字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ],
+      },
+      {
+        // 加载文件
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ],
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new CleanWebpackPlugin(),
+  ]
 };
